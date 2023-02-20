468bff5adfe1b2573e4b8c7d4e1d347c4fae3309
PJazminCastro <121037801@upq.edu.mx>
2023-02-20 15:43:26 -0600

    Se crea el personaje de "Heroe"

---
+++ Objetos.py
@@ -0,0 +1,9 @@
+from Personaje import * #* = importa toda la clase.
+
+#Creamos una instancia de la clase Personaje
+Heroe = Personaje()
+
+#Usamos los atributos 
+print("El personaje se llama "+Heroe.nombre)
+print("Pertenece a la especie "+Heroe.especie)
+print("Tiene una altura de  "+str(Heroe.altura))
