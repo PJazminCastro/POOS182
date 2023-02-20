e343f238a50ba8825547a5cab397f4860f787517
PJazminCastro <121037801@upq.edu.mx>
2023-02-20 15:37:54 -0600

    Se crearon los métodos "lanzar granadas" y "recargar arma"

--- Personaje.py
+++ Personaje.py
@@ -8,5 +8,13 @@ class Personaje:
     #Métodos
     def Correr(self, status):
         if(status): 
-            print("El personaje "+self.nombre+" esta corriendo")
-        else: print("El personaje "+self.nombre+" se detuvo")
+            print("El personaje "+self.nombre+" esta corriendo.")
+        else: print("El personaje "+self.nombre+" se detuvo.")
+
+    def LanzarGranadas(self):
+        print("El personaje "+self.nombre+" lanzo una granada.")
+
+    def RecargarArma(self, municiones):
+        cargador = 10
+        cargador = cargador + municiones
+        print("El arma recargada tiene "+cargador+" balas.")
