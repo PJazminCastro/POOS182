d2dab112f5e8d7668c28b023334babcf4b7f92c0
PJazminCastro <121037801@upq.edu.mx>
2023-02-22 15:33:28 -0600

    Ejemplificador del contructor

--- Objetos.py
+++ Objetos.py
@@ -1,14 +1,41 @@
 from Personaje import * #* = importa toda la clase.
+#2. Solicitamos los parametros.
+print('¡Solicitud de datos para el heroe!')
+especieH = input('Escribe la especie del heroe: ')
+nombreH = input('Escribe el nombre del heroe: ')
+alturaH = float(input('Escribe la altura del heroe: '))
+recargaH = int(input('Escriba cuantas balas desea que tenga el heroe: '))
+print('')
+print('¡Solicitud de datos para el villano!')
+especieV = input('Escribe la especia del villano: ')
+nombreV = input('Escribe el nombre del villano: ')
+alturaV = float(input('Escribe la altura del villano: '))
+recargaV = int(input('Escriba cuantas balas desea que tenga el villano: '))
 
-#Creamos una instancia de la clase Personaje
-Heroe = Personaje()
+#3. Creamos los objetos
+Heroe = Personaje(especieH, nombreH, alturaH)
+Villano = Personaje(especieV, nombreV, alturaV)
 
-#Usamos los atributos 
+#4. Usamos los atributos del heroe y villano
+print('')
+print('¡HEROE!')
 print("El personaje se llama "+Heroe.nombre)
 print("Pertenece a la especie "+Heroe.especie)
 print("Tiene una altura de  "+str(Heroe.altura))
-
-#Usar los métodos
 Heroe.Correr(True)
 Heroe.LanzarGranadas()
-Heroe.RecargarArma(37)
+Heroe.RecargarArma(recargaH)
+
+
+print('')
+print('¡VILLANO!')
+print("El personaje se llama "+Villano.nombre)
+print("Pertenece a la especie "+Villano.especie)
+print("Tiene una altura de  "+str(Villano.altura))
+Villano.Correr(True)
+Villano.LanzarGranadas()
+Villano.RecargarArma(recargaV)
+
+#5. Usar los métodos
+
+
--- Personaje.py
+++ Personaje.py
@@ -1,9 +1,9 @@
 class Personaje:
-
-    #Atributos
-    especie = "Humano"
-    nombre = "MasterChief"
-    altura = 2.70
+    #1. Constructor
+    def __init__(self, esp, nom, alt):
+        self.especie = esp
+        self.nombre = nom
+        self.altura = alt
 
     #Métodos
     def Correr(self, status):
