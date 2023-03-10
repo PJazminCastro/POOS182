15ab81feeac19910e2b01f4382bfb9edb7c31528
PJazminCastro <121037801@upq.edu.mx>
2023-03-10 11:40:08 -0600

    Correcciones a Practica 13

--- Practicas_P2/DatosP13.py
+++ Practicas_P2/DatosP13.py
@@ -3,32 +3,41 @@ import secrets; import string; import tkinter as tk;
 class DatosP13:
 
     def __init__(self, longitud, mayusc, caractE):
-        self.__longitud = tk.StringVar()
+        self.__longitud = 12
         self.__mayuc = mayusc
         self.__caractE = caractE
 
     def Generar(self):
         #validar que longitud sea >=8
-        self.__longitud = tk.StringVar()
-        longi= int(self.__longitud.get())
-        if(longi >= 8):
-            mayusc = messagebox.askyesno('Mayusculas', '¿Desea que su contraseña cuente con mayusculas?')
-            print(mayusc)
-            caractE = messagebox.askyesno('Caracter especial', '¿Desea que contenga caracteres especiales (@, _, -, *, +)?')
-            print(caractE)
+            print(self.__longitud)
+            print(self.__mayuc)
+            print(self.__caractE)
             #Validar si quiere mayuculas
-            if(mayusc == True):
+            if(self.__mayuc.get() == 'Si'):
                 Mayuscula = string.ascii_letters
                 alfabeto = Mayuscula
                 #validar si quiere caracteres
-                if(caractE == True):
+                if(self.__caractE.get() == 'Si'):
                     Caracter = string.punctuation
                     alfabeto = Mayuscula + Caracter
+                    pdw = ''
+                    for i in range(self.__longitud):
+                        pdw += ''.join(secrets.choice(alfabeto))
+                    messagebox.showinfo('Contraseña',pdw)
+                else: 
+                    alfabeto = Mayuscula
+                    pdw = ''
+                    for i in range(self.__longitud):
+                        pdw += ''.join(secrets.choice(alfabeto))
+                    messagebox.showinfo('Contraseña',pdw)
             else:
-                if(caractE == True):
+                if(self.__caractE.get() == 'Si'):
                     Caracter = string.punctuation
                     alfabeto = Caracter
+                    pdw = ''
+                    for i in range(self.__longitud):
+                        pdw += ''.join(secrets.choice(alfabeto))
+                    messagebox.showinfo('Contraseña', pdw)
                 else:
                     messagebox.showerror('Error', 'Debe contener minimo alguna mayuscula o caracter especial')  
-        else:
-            messagebox.showerror('Error','La longitud minima es de 8 caracteres.')
+        
